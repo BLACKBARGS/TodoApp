a2cc4b1e4624dea17fc22ea6d1c123108e146237
Francys Rodrigues <119719373+BLACKBARGS@users.noreply.github.com>
2023-02-09 11:38:02 -0300

    Changes to be committed: 	new file:   Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs 	new file:   Todo.Domain.Tests/EntityTests/TodoItemTests.cs 	new file:   Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs 	new file:   Todo.Domain.Tests/QueryTests/TodoQueriesTests.cs 	new file:   Todo.Domain.Tests/Repositories/FakeTodoRepository.cs 	deleted:    Todo.Domain.Tests/UnitTest1.cs 	modified:   Todo.Domain/Commands/GenericCommandResult.cs 	modified:   Todo.Domain/Entities/TodoItem.cs 	new file:   Todo.Domain/Queries/TodoQueries.cs

--- Todo.Domain/Entities/TodoItem.cs
+++ Todo.Domain/Entities/TodoItem.cs
@@ -9,7 +9,7 @@ public class TodoItem : Entity
         Date = date;
     }
     //?? WTF
-    public TodoItem(string? title, bool? done, DateTime? date, string? user)
+    public TodoItem(string? title, bool? done, DateTime date, string? user)
     {
         Title = title;
         Done = done;
@@ -19,7 +19,7 @@ public class TodoItem : Entity
 
     public string? Title {get; private set;}
     public bool? Done {get; private set;}
-    public DateTime? Date {get; private set;}
+    public DateTime Date {get; private set;}
     public string? User {get; private set;}
 
     public void MarkAsDone()
