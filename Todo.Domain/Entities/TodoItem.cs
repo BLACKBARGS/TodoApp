f1e98764fc83b4b38f8fea77c7078cdb861a966f
Francys Rodrigues <119719373+BLACKBARGS@users.noreply.github.com>
2023-02-09 10:20:43 -0300

    modified:   Todo.Domain.Api/obj/Debug/net7.0/project.razor.json 	modified:   Todo.Domain/Entities/TodoItem.cs 	new file:   Todo.Domain/Handlers/TodoHandler.cs#

--- Todo.Domain/Entities/TodoItem.cs
+++ Todo.Domain/Entities/TodoItem.cs
@@ -1,10 +1,15 @@
-using System;
-
 namespace Todo.Domain.Entities;
 
 public class TodoItem : Entity
 {
-    public TodoItem(string? title, bool done, DateTime date, string? user)
+    public TodoItem(string? title, string? user, DateTime date)
+    {
+        Title = title;
+        User = user;
+        Date = date;
+    }
+    //?? WTF
+    public TodoItem(string? title, bool? done, DateTime? date, string? user)
     {
         Title = title;
         Done = done;
@@ -13,8 +18,8 @@ public class TodoItem : Entity
     }
 
     public string? Title {get; private set;}
-    public bool Done {get; private set;}
-    public DateTime Date {get; private set;}
+    public bool? Done {get; private set;}
+    public DateTime? Date {get; private set;}
     public string? User {get; private set;}
 
     public void MarkAsDone()
